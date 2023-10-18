b4d6240f7da4e166cefda1c383f3d9cffd184287
Will <willgraham345@gmail.com>
2023-10-18 16:28:44 -0600

    Refactored Superisor thread call within p_thread. Needs work.

--- lib/p_thread/src/p_thread.c
+++ lib/p_thread/src/p_thread.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
+#include <zephyr.h>
+#include <arch/cpu.h>
+#include <unity.h>
 #include "p_thread.h"
 
 
-void thread_entry()
-{
-    for (int i = 0; ; i++);
-}
 
-void thread_create_wrapper(struct k_thread * new_thread, k_thread_stack_t * stack, size_t stack_size, int priority, k_timeout_t wait)
+
+void thread_create_wrapper(struct k_thread * new_thread,
+    k_thread_stack_t * stack,
+    size_t stack_size, 
+    int priority,
+    k_timeout_t wait)
 {
+    // TODO: Refactor this into run_analyzer_split. 
     k_thread_create(new_thread,
                     stack,
                     stack_size,
@@ -15,20 +21,25 @@ void thread_create_wrapper(struct k_thread * new_thread, k_thread_stack_t * stac
                     NULL,
                     NULL,
                     NULL,
-                    K_PRIO_COOP(priority),
+                    priority,
                     0,
                     wait);
 
 }
 
-void supervisor_create_wrapper(struct k_thread * new_thread, k_thread_stack_t * stack, size_t stack_size)
+void supervisor_create_wrapper(struct k_thread * supervisor_thread, 
+    k_thread_stack_t * supervisor_stack, 
+    size_t stack_size, 
+    k_thread_entry_t supervisor_entry, 
+    struct k_thread * hi_prio_thread,
+    struct k_thread * low_prio_thread)
 {
-        k_thread_create(new_thread,
-                    stack,
+        k_thread_create(supervisor_thread,
+                    stacksupervisor_stack,
                     stack_size,
                     (k_thread_entry_t) supervisor_entry,
-                    NULL,
-                    NULL,
+                    hi_prio_thread,
+                    lo_prio_thread,
                     NULL,
                     -CONFIG_NUM_COOP_PRIORITIES,
                     0,
@@ -37,7 +48,61 @@ void supervisor_create_wrapper(struct k_thread * new_thread, k_thread_stack_t * 
 
 
 
-void supervisor_entry()
+void run_analyzer(k_thread_entry_t thread_entry,
+                  void *arg,
+                  int pri_prio,  k_timeout_t pri_delay,
+                  uint64_t *pri_duration,
+                  int sec_prio,  k_timeout_t sec_delay,
+                  uint64_t *sec_duration,
+                  uint64_t *total_duration)
 {
-    for (int i = 0; ; i++);
+    run_analyzer_split(5000,
+                       thread_entry,
+                       primary_name, arg, NULL,
+                       pri_prio, pri_delay, pri_duration,
+                       thread_entry,
+                       secondary_name, arg, NULL,
+                       sec_prio, sec_delay, sec_duration,
+                       total_duration);
+
+}
+
+
+
+void run_analyzer_split(uint32_t test_duration,
+                        k_thread_entry_t pri_thread_entry,
+                        void *pri_arg0, void *pri_arg1, void *pri_arg2,
+                        int pri_prio,  k_timeout_t pri_delay,
+                        uint64_t *pri_duration,
+
+                        k_thread_entry_t sec_thread_entry,
+                        void *sec_arg0, void *sec_arg1, void *sec_arg2,
+                        int sec_prio,  k_timeout_t sec_delay,
+                        uint64_t *sec_duration,
+
+                        uint64_t *total_duration)
+{
+
+	uint64_t start, primary, secondary, end, elapsed;
+    k_thread_runtime_stats_t pri_stats, sec_stats, start_stats, end_stats;;
+
+    k_thread_runtime_stats_all_get(&start_stats);
+    supervisor_create_wrapper(&super_thread, 
+                            super_stack,
+                            STACKSIZE,
+                            (k_thread_entry_t) super_entry,
+                            &hi_prio_thread,
+                            &low_prio_thread,
+                            NULL);
+
+    //TODO: Add two preemtive thread calls
+    //TODO: Add joining threads
+    //TODO: Add runtime states
+    //TODO: Add printing everything (use printk)
+
+    //TODO: Add aborting threads.
+
+    // thread_create_wrapper(&primary_)
+
+
 }
--- test/test_threads/test_threads.c
+++ test/test_threads/test_threads.c
@@ -32,6 +32,8 @@ void busy_sleep(char *name)
 
 void priority_inversion(char *name, struct k_sem *sem)
 {
+    // Tests if a lower priority thread will keep a higher priority thread from the semaphore.
+    // Think about when to test the runtime
     printk("start priority inversion %s\n", name);
     k_sem_take(sem, K_FOREVER);
     printk("got semaphore %s\n", name);
@@ -41,10 +43,17 @@ void priority_inversion(char *name, struct k_sem *sem)
 // Activity 1
 void test_priority_inversion(void)
 {
-    printk("Starting prioirty inversion test %s\n", name);
-    k_sem_take()
-}
+    uint64_t high_stats, low_stats, elapsed_stats;
+    struct k_sem sem;
+    k_sem_init(&sem, 1, 1);
 
+    run_analyzer((k_thread_entry_t)priority_inversion, &sem,
+                 K_PRIO_PREEMPT(4), K_MSEC(10), &low_stats,
+                 K_PRIO_PREEMPT(3), K_MSEC(12), &high_stats,
+                 &elapsed_stats);
+    TEST_ASSERT_UINT64_WITHIN(1000, 0, high_stats);
+    TEST_ASSERT_UINT64_WITHIN(100000, 5000000, low_stats);
+}
 
 
 
